be12c71e103a96a9fbe8cd230c7be2fdb4261fc9
liubaohu <liubaohu_LBH@163.com>
2025-05-23 15:15:24 +0800

    init

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>这是头</h1>
+    <main class="content-container" ref="container">
+      <nuxt />
+    </main>
+    <h1>这是尾</h1>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const version = 2 + 1;
+</script>
+
+<template>
+  <div class="hello">这是index</div>
+</template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
